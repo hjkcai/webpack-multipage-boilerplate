ac0b6be11dc526877ed587a945ade15e3358779d
hjkcai <huajingkun1214@gmail.com>
2017-03-14 17:05:28 +0800

    apply webpack's hot update

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -3,7 +3,12 @@
 const merge = require('webpack-merge')
 const config = require('../config')
 const webpack = require('webpack')
+const webpackConfig = require('./webpack.base.conf')
 const FriendlyErrors = require('friendly-errors-webpack-plugin')
+
+Object.keys(webpackConfig.entry).forEach(key => {
+  webpackConfig.entry[key] = ['webpack-hot-middleware/client?reload=true', webpackConfig.entry[key]]
+})
 
 module.exports = merge(require('./webpack.base.conf'), {
   output: {
