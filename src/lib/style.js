6911b148226b2f7fed1d6244c31f445bf320031a
hjkcai <huajingkun1214@gmail.com>
2017-03-15 13:42:21 +0800

    update example

---
+++ src/lib/style.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// Place other JavaScript modules in the 'lib' folder
+import 'styles/index.less'
+console.log('Style applied')
+
+export default 'style module'
---
+++ src/scripts/index.js
@@ -0,0 +1,4 @@
+'use strict'
+
+import '../lib/style'
+console.log('Hello World')
